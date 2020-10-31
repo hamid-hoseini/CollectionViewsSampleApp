8fe6ede08b0d8af7b9fcc65842f7c4059d02f83d
Hamid <mail.hamid.hoseini@gmail.com>
2020-10-30 22:11:41 -0700

    first commit

---
+++ CollectionViews/PhotoCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCollectionViewCell.swift
+//  CollectionViews
+//
+//  Created by Hamid Hoseini on 10/30/20.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
